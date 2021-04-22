24fe6b197176d0bca83bc045cd5a7b020cccd1a4
GioOli7 <gio07live@gmail.com>
2021-04-22 17:45:26 +0200

    edit task from input

--- js/script.js
+++ js/script.js
@@ -5,21 +5,30 @@ const app = new Vue({
             {
                 text: 'Comprare il pane',
                 completed: false,
+                isEditing: false,
+                inputEditTask: '',
             },
             {
                 text: 'Chiamare il sindacalista',
                 completed: false,
+                isEditing: false,
+                inputEditTask: '',
             },
             {
                 text: 'Pagare le bollette',
                 completed: false,
+                isEditing: false,
+                inputEditTask: '',
             },
             {
                 text: 'Pulire casa',
                 completed: true,
+                isEditing: false,
+                inputEditTask: '',
             },
         ],
         inputText: '',
+        isEditing: false,
         
     },
     methods: {
@@ -31,6 +40,7 @@ const app = new Vue({
                 this.todos.push({
                     text: this.inputText,
                     completed: false,
+                    isEditing: false,
                 })
             }
             this.inputText = '';
@@ -44,8 +54,27 @@ const app = new Vue({
             this.todos.splice(index, 1);
         },
 
+        /**
+         * toggle between completed and uncompleted task
+         */
         taskDone(index) {
             this.todos[index].completed = ! this.todos[index].completed;
+        },
+
+        editTask(index) {
+            if (this.todos[index].isEditing === false) {
+                this.todos[index].isEditing = ! this.todos[index].isEditing;
+                this.todos[index].inputEditTask = this.todos[index].text;
+            } else {
+                this.todos[index].text = this.todos[index].inputEditTask;
+                this.todos[index].isEditing = false;
+            }
+            
+        },
+        confirmEditTask(index) {
+            this.todos[index].text = this.todos[index].inputEditTask;
+            this.todos[index].isEditing = false;
         }
+
     }
 })

a36900d2a880e84d9d121f52f9a8c1289cf31b5b
GioOli7 <gio07live@gmail.com>
2021-04-22 22:34:57 +0200

    Fix / code cleaning

--- js/script.js
+++ js/script.js
@@ -63,30 +63,6 @@ const app = new Vue({
         },
 
         /**
-         * Allow editing task from icon click
-         */
-        editTask(index) {
-            // se la "modalità modifica" è disabilitata, viene abilitata e il testo della task
-            // viene copiata nella input, pronta per la modifica
-            if (this.todos[index].isEditing === false) {
-                this.todos[index].isEditing = true;
-                this.todos[index].inputEditTask = this.todos[index].text;
-                // altrimenti se si è già in modifica, il testo della input diventa il testo della task
-                // e la "modalità modifica" viene chiusa.
-            } else {
-                // prevengo che venga salvata una task senza testo
-                if (this.todos[index].inputEditTask == '') {
-                    this.todos[index].inputEditTask = this.todos[index].text;
-                }
-                this.todos[index].text = this.todos[index].inputEditTask;
-                this.todos[index].isEditing = false;
-                // in questo modo posso salvare la modifica sia ricliccando sull'icona
-                // e sia premendo il tasto invio dalla tastiera (funzione confirmEditTask(index))
-            }
-        
-        },
-
-        /**
          * Confirm editing task from enter key
          */
         confirmEditTask(index) {
@@ -98,7 +74,25 @@ const app = new Vue({
             // e la "modalità modifica" viene chiusa.
             this.todos[index].text = this.todos[index].inputEditTask;
             this.todos[index].isEditing = false;
-        }
+        },
 
+        /**
+         * Allow editing task from icon click
+         */
+        editTask(index) {
+            // se la "modalità modifica" è disabilitata, viene abilitata e il testo della task
+            // viene copiata nella input, pronta per la modifica
+            if (this.todos[index].isEditing === false) {
+                this.todos[index].isEditing = true;
+                this.todos[index].inputEditTask = this.todos[index].text;
+                // altrimenti se si è già in modifica, il testo della input diventa il testo della task
+                // e la "modalità modifica" viene chiusa.
+            } else {
+                // RICORDA!! USA THIS ANCHE PER RICHIAMARE ALTRE FUNZIONI!!!
+                this.confirmEditTask(index);    
+                // in questo modo posso salvare la modifica sia ricliccando sull'icona
+                // e sia premendo il tasto invio dalla tastiera (funzione confirmEditTask(index))
+            }
+        },
     }
 })
